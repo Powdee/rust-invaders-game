1df2e83be09700db90e654975eeac66260ffd916
ekurjak <ekurjak@alteryx.com>
2022-05-21 19:00:52 +0200

    create render , frane mod and lib

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 	audio.play("startup");
 
 	// terminal
-
 	let mut stdout = io::stdout();
 	terminal::enable_raw_mode()?;
 	stdout.execute(EnterAlternateScreen)?;
 	stdout.execute(Hide)?;
 
 	// game loop
-
 	'gameloop: loop {
 		// input
 		while event::poll(Duration::default())? {
@@ -50,7 +48,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 	}
 
 	// cleanup
-
 	audio.wait();
 	stdout.execute(Show)?;
 	stdout.execute(LeaveAlternateScreen)?;
