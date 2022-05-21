aa46010d1c05ea80dbb20646e76bdfe253f8ced5
ekurjak <ekurjak@alteryx.com>
2022-05-21 18:45:27 +0200

    add game loop and add escape key code to lose the game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use crossterm::event::{Event, KeyCode};
 use rusty_audio::Audio;
 use std::error::Error;
+use std::time::Duration;
 
-use crossterm::terminal;
+use crossterm::{terminal, event};
 use std::io;
 
 use crossterm::cursor::{Hide, Show};
@@ -11,6 +13,7 @@ use crossterm::ExecutableCommand;
 fn main() -> Result<(), Box<dyn Error>> {
 	let mut audio = Audio::new();
 
+	// set audio
 	audio.add("explode", "explode.wav");
 	audio.add("lose", "lose.wav");
 	audio.add("move", "move.wav");
@@ -21,12 +24,33 @@ fn main() -> Result<(), Box<dyn Error>> {
 	audio.play("startup");
 
 	// terminal
+
 	let mut stdout = io::stdout();
 	terminal::enable_raw_mode()?;
 	stdout.execute(EnterAlternateScreen)?;
 	stdout.execute(Hide)?;
 
+	// game loop
+
+	'gameloop: loop {
+		// input
+		while event::poll(Duration::default())? {
+			if let Event::Key(key_event) = event::read()? {
+				match key_event.code {
+					// escape button will perform audio from lose track
+					KeyCode::Esc | KeyCode::Char('q') => {
+						audio.play("lose");
+						break 'gameloop;
+					},
+					// empty function 
+					_ => {}
+				}
+			}
+		}
+	}
+
 	// cleanup
+
 	audio.wait();
 	stdout.execute(Show)?;
 	stdout.execute(LeaveAlternateScreen)?;
