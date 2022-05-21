4984ab8c07395c09fe67297eabea014807b21f19
ekurjak <ekurjak@alteryx.com>
2022-05-21 18:33:53 +0200

    add terminal to show and hide screen and then cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use rusty_audio::Audio;
 use std::error::Error;
 
+use crossterm::terminal;
+use std::io;
+
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::ExecutableCommand;
+
 fn main() -> Result<(), Box<dyn Error>> {
 	let mut audio = Audio::new();
 
@@ -14,9 +21,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 	audio.play("startup");
 
 	// terminal
+	let mut stdout = io::stdout();
+	terminal::enable_raw_mode()?;
+	stdout.execute(EnterAlternateScreen)?;
+	stdout.execute(Hide)?;
 
 	// cleanup
 	audio.wait();
+	stdout.execute(Show)?;
+	stdout.execute(LeaveAlternateScreen)?;
+
+	terminal::disable_raw_mode()?;
 
 	return Ok(());
 }
