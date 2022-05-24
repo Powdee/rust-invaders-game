732a3c8fef91f234715ab0d1aa99897c14c81fe3
ekurjak <ekurjak@alteryx.com>
2022-05-24 22:57:19 +0200

    Create and render player plus working on shots - still not finished

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use crossterm::event::{Event, KeyCode};
 use invaders::{
-	frame::{self, new_frame},
-	render
+    frame::{self, new_frame},
+    player::Player,
+    render,
 };
 use rusty_audio::Audio;
 use std::error::Error;
 use std::sync::mpsc;
 use std::time::Duration;
 
-use crossterm::{terminal, event};
+use crossterm::{event, terminal};
 use std::{io, thread};
 
 use crossterm::cursor::{Hide, Show};
@@ -16,77 +17,80 @@ use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::ExecutableCommand;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let mut audio = Audio::new();
+    let mut audio = Audio::new();
 
-	// set audio
-	audio.add("explode", "explode.wav");
-	audio.add("lose", "lose.wav");
-	audio.add("move", "move.wav");
-	audio.add("pew", "pew.wav");
-	audio.add("startup", "startup.wav");
-	audio.add("win", "win.wav");
+    // set audio
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
 
-	audio.play("startup");
+    audio.play("startup");
 
-	// terminal
-	let mut stdout = io::stdout();
-	terminal::enable_raw_mode()?;
-	stdout.execute(EnterAlternateScreen)?;
-	stdout.execute(Hide)?;
+    // terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
 
-	// render loop in a separate thread
-	let (render_tx, render_rx) = mpsc::channel();
-	let render_handle = thread::spawn(move || {
-		let mut last_frame = frame::new_frame();
-		let mut stdout = io::stdout();
+    // render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
 
-		render::render(&mut stdout, &last_frame, &last_frame, true);
+        render::render(&mut stdout, &last_frame, &last_frame, true);
 
-		loop {
-			let curr_frame = match render_rx.recv() {
-				Ok(x) => x,
-				Err(_) => break
-			};
-			render::render(&mut stdout, &last_frame, &curr_frame, false);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
 
-			last_frame = curr_frame;
-		}
-	});
+            last_frame = curr_frame;
+        }
+    });
 
-	// game loop
-	'gameloop: loop {
-		// per frame init
-		let curr_frame = new_frame();
+    // game loop
+    let mut player = Player::new();
+    'gameloop: loop {
+        // per frame init
+        let mut curr_frame = new_frame();
 
+        // input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    // escape button will perform audio from lose track
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    // empty function
+                    _ => {}
+                }
+            }
+        }
 
-		// input
-		while event::poll(Duration::default())? {
-			if let Event::Key(key_event) = event::read()? {
-				match key_event.code {
-					// escape button will perform audio from lose track
-					KeyCode::Esc | KeyCode::Char('q') => {
-						audio.play("lose");
-						break 'gameloop;
-					},
-					// empty function 
-					_ => {}
-				}
-			}
-		}
+        // draw and render
+        frame::Drawable::draw(&player, &mut curr_frame);
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
 
-		// draw and render
-		let _ = render_tx.send(curr_frame);
-		thread::sleep(Duration::from_millis(1));
-	}
+    // cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
 
-	// cleanup
-	drop(render_tx);
-	render_handle.join().unwrap();
-	audio.wait();
-	stdout.execute(Show)?;
-	stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
-	terminal::disable_raw_mode()?;
-
-	return Ok(());
+    return Ok(());
 }
---
+++ src/shot.rs
@@ -0,0 +1,36 @@
+use std::time::Duration;
+
+use rusty_time::prelude::Timer;
+
+pub struct Shot {
+    pub x: usize,
+    pub y: usize,
+    pub exploding: bool,
+    timer: Timer,
+}
+
+impl Shot {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self {
+            x,
+            y,
+            exploding: false,
+            timer: Timer::from_millis(50),
+        }
+    }
+
+    pub fn update(&mut self, delta: Duration) {
+        self.timer.update(delta);
+        if self.timer.ready && !self.exploding {
+            if self.y > 0 {
+                self.y -= 1;
+            }
+            self.timer.reset();
+        }
+    }
+
+    pub fn explode(&mut self) {
+        self.exploding = true;
+        self.timer = Timer::from_millis(250);
+    }
+}
